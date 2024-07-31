ebded31d1bf1b356108b4260eadf0619d33d46a5
Daniil Bobrov <allodrmen@yandex.ru>
2024-07-31 18:05:57 +0300

    renamed component

---
+++ src/components/QuestionNode.vue
@@ -0,0 +1,14 @@
+<template>
+    <h4 class="question-node-question">Вопрос 1</h4>
+    <input v-model="question" type="text"/>
+    <hr>
+    <button type="button">Добавить ответ</button>
+</template>
+
+<script setup lang="ts">
+const question = ref<string>('');
+</script>
+
+<style scoped>
+
+</style>
