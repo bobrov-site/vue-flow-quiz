5f641e6cb8099a7baa583435da7b9190f16fb5de
Daniil Bobrov <allodrmen@yandex.ru>
2024-08-01 15:01:27 +0300

    remove initialEdges

--- initial-elements.js
+++ initial-elements.js
@@ -1,5 +1,3 @@
-import { MarkerType } from '@vue-flow/core'
-
 export const initialNodes = [
   {
     id: '1',
@@ -10,34 +8,4 @@ export const initialNodes = [
   },
 ]
 
-export const initialEdges = [
-  // {
-  //   id: 'e1-2',
-  //   source: '1',
-  //   target: '2',
-  //   animated: true,
-  // },
-  // {
-  //   id: 'e1-3',
-  //   source: '1',
-  //   target: '3',
-  //   label: 'edge with arrowhead',
-  //   markerEnd: MarkerType.ArrowClosed,
-  // },
-  // {
-  //   id: 'e4-5',
-  //   type: 'step',
-  //   source: '4',
-  //   target: '5',
-  //   label: 'Node 2',
-  //   style: { stroke: 'orange' },
-  //   labelBgStyle: { fill: 'orange' },
-  // },
-  // {
-  //   id: 'e3-4',
-  //   type: 'smoothstep',
-  //   source: '3',
-  //   target: '4',
-  //   label: 'smoothstep-edge',
-  // },
-]
+export const initialEdges = []
