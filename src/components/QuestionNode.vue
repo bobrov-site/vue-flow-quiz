a0601cdcc9c2350ae8a0145e85ea50a15c806bba
Daniil Bobrov <allodrmen@yandex.ru>
2024-08-01 11:12:52 +0300

    change question node

--- src/components/QuestionNode.vue
+++ src/components/QuestionNode.vue
@@ -1,14 +1,16 @@
 <template>
-    <h4 class="question-node-question">Вопрос 1</h4>
-    <input v-model="question" type="text"/>
-    <hr>
-    <button type="button">Добавить ответ</button>
+    <div class="node-question node">
+        <h4 class="node-title">Вопрос 1</h4>
+        <input v-model="question" class="node-input" placeholder="Является ли ваше произведение ПО?" type="text"/>
+        <button class="node-button node-button-green" type="button">Добавить ответ</button>
+    </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 const question = ref<string>('');
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
