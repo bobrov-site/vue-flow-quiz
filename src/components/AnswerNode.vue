59b259c77cce2f1986d007218fe020a642e21163
Daniil Bobrov <allodrmen@yandex.ru>
2024-08-01 18:00:42 +0300

    added answerNode actions

--- src/components/AnswerNode.vue
+++ src/components/AnswerNode.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="node-answer node">
-        <h4 class="node-title">Ответ 2 на вопрос 3</h4>
-        <input v-model="answer" class="node-input" placeholder="Да" type="text"/>
+        <h4 class="node-title">{{ props.data.title }}</h4>
+        <input v-model="answer" class="node-input" type="text"/>
         <div class="node-licenses-wrapper">
             <div class="node-license-header">
                 <span class="node-license-title">Лицензия</span>
@@ -9,20 +9,18 @@
             </div>
         </div>
         <div class="node-licenses-list">
-            <div class="node-license-item">
-                <select class="node-license-select">
-                    <option>MIT</option>
-                    <option>Apache2</option>
-                    <option>Apache3</option>
+            <div v-for="license in selectedLicenses" :key="license.id" class="node-license-item">
+                <select v-model="license.name" class="node-license-select">
+                    <option v-for="item in licenses" :key="item.id" :value="item.name" :disabled="isDisabled(item)">{{ item.name}}</option>
                 </select>
-                <select class="node-license-select">
+                <select v-model="license.weight" class="node-license-select">
                     <option value="1">1</option>
                     <option value="0">0</option>
                     <option value="null">null</option>
                 </select>
             </div>
         </div>
-        <button class="node-button" type="button">+ Добавить лицензию</button>
+        <button @click="addLicense" class="node-button" type="button">+ Добавить лицензию</button>
         <hr class="node-hr">
         <button class="node-button node-button-green mb-12">Добавить вопрос</button>
         <button class="node-button node-button-green">Результат</button>
@@ -30,9 +28,49 @@
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue';
-
+import {onMounted, ref} from 'vue';
+import { useVueFlow } from '@vue-flow/core'
+import type { NodeProps } from '@vue-flow/core';
+import type { License } from '../types.ts';
+import api from '../api';
+const { getNodes, getEdges, addNodes} = useVueFlow();
 const answer = ref<string>('');
+const props = defineProps<NodeProps>()
+const licenses = ref<License[] | []>()
+const selectedLicenses = ref<License[]>([]);
+const disabledLicenses = ref<License[]>([]);
+onMounted(async() => {
+    answer.value = props.data.text;
+    await fetchLicenses();
+})
+
+const fetchLicenses = async() => {
+    const response = await api.fetchLicenses();
+    licenses.value = response.map((licence: License) => {
+        const newLicense = {
+            id: licence.id,
+            name: licence.name,
+            weight: '0'
+        }
+        return newLicense;
+    })
+    selectedLicenses.value.push(licenses.value[0])
+}
+
+const isDisabled = (license: License): boolean => {
+    return disabledLicenses.value.some((item) => item.name === license.name)
+}
+
+const addLicense = () => {
+    disabledLicenses.value.push(selectedLicenses.value[selectedLicenses.value.length - 1])
+    const newLicense: License = {
+        id: 0,
+        name: '',
+        weight: '0'
+    }
+    selectedLicenses.value.push(newLicense);
+}
+
 </script>
 
 <style scoped>
