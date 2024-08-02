0fe0be2b8e33a66f39b670a01ac64a41d68c647f
Daniil Bobrov <allodrmen@yandex.ru>
2024-08-02 14:18:44 +0300

    show licenses result

--- src/components/ResultNode.vue
+++ src/components/ResultNode.vue
@@ -9,17 +9,25 @@
             </div>
         </div>
         <div class="node-licenses-list">
-            <div class="node-license-item">
-                <div class="node-license-item-data">MIT</div>
-                <div class="node-license-item-data">null</div>
+            <div v-for="license in licenses" :key="license.id" class="node-license-item">
+                <div class="node-license-item-data">{{ license.name }}</div>
+                <div class="node-license-item-data node-license-item-count">{{ license.weight }}</div>
             </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import {ref} from 'vue';
+import {onMounted, ref} from 'vue';
+import type { NodeProps } from '@vue-flow/core';
+import type { License } from '@/types';
+const props = defineProps<NodeProps>()
+const licenses = ref<License[] | []>()
 const result = ref<string>('');
+onMounted(() => {
+    result.value = props.data.text;
+    licenses.value = props.data.licenses;
+})
 </script>
 
 <style scoped>
