2971e810cf4f35fbef130dc0005b63f5626f785d
Daniil Bobrov <allodrmen@yandex.ru>
2024-08-09 09:12:22 +0300

    change route

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,8 @@ const domain = 'https://choosealicense.dev.rusrobots.ru';
 const routes = {
     baseUrl: `${domain}/api`,
     licensesDictionary: '/licenses/dictionary',
-    quiz: '/quiz',
+    quiz: '/quiz/active',
+    quizId: (id:number) => `/quiz/${id}`, 
 }
 
 export default routes;
