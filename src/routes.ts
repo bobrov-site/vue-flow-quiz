7474f1b93e9da43d5f8ec2bf6aea9e2590541e06
Daniil Bobrov <allodrmen@yandex.ru>
2024-08-20 17:17:49 +0300

    added welcome route

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ const domain = 'https://choosealicense.dev.rusrobots.ru'; //YOUR DOMAIN
 
 const routes = {
     baseUrl: `${domain}/api`,
+    welcome: '/welcome',
     licensesDictionary: '/licenses/dictionary',
     quiz: '/quiz/active',
     quizId: (id:number) => `/quiz/${id}/update`, 
